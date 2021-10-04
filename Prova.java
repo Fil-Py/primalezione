10b6322e3a8428cb3d36e620b0159762942f63ce
UranusDarkness <uranusdarkness@gmail.com>
2021-10-04 12:58:40 +0200

    Push

---
+++ Prova.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Prova{
+  public static void main(String[] args){
+    Scanner tastiera = new Scanner(System.in);
+
+    System.out.print("Inserisci valore intero: ");
+    int pippo = tastiera.nextInt();
+    System.out.print("Hai inserito il valore: "+pippo);
+
+    int n1, n2, numero;
+  }
+}
